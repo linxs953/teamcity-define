19dc6309f50be2f3937ff2c7b34d11f713bb5181
linxs <cakelinxs@outlook.com>
2026-03-19 14:41:13 +0800

    更新文件

--- .teamcity/project.kt
+++ .teamcity/project.kt
@@ -1,14 +1,24 @@
-import jetbrains.buildServer.configs.kotlin.v2019_2.*
-import jetbrains.buildServer.configs.kotlin.v2019_2.projectFeatures.VersionedSettings
-import jetbrains.buildServer.configs.kotlin.v2019_2.projectFeatures.versionedSettings
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildFeatures.XmlReport
+import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
+import jetbrains.buildServer.configs.kotlin.buildFeatures.xmlReport
+import jetbrains.buildServer.configs.kotlin.buildSteps.DotnetMsBuildStep
+import jetbrains.buildServer.configs.kotlin.buildSteps.dotCover
+import jetbrains.buildServer.configs.kotlin.buildSteps.dotnetMsBuild
+import jetbrains.buildServer.configs.kotlin.buildSteps.powerShell
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.triggers.finishBuildTrigger
+import jetbrains.buildServer.configs.kotlin.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.projectFeatures.VersionedSettings
+import jetbrains.buildServer.configs.kotlin.projectFeatures.versionedSettings
 
 version = "2024.03"
 
 project {
-    name = "TeamCity Define Project"
+    name = "TcDemo Project"
 
-    buildType(UnitTests)
-    buildType(BuildAndPackage)
+    buildType(DemoPipeline)
+    buildType(TcDemo_Build2)
 
     features {
         versionedSettings {
@@ -24,48 +34,119 @@ project {
     params {
         param("system.Gradle_Version", "8.5")
     }
+
+    // VCS Root definition - 需要根据实际情况配置
+    subProject(TcDemoVcs)
 }
 
-object UnitTests : BuildType({
-    name = "Unit Tests"
-    id = absoluteId("UnitTests")
+object TcDemoVcs : Project({
+    name = "VCS Roots"
 
-    vcs {
-        root(DslContext.settingsRoot)
-    }
+    vcsRoot(TcDemo)
+})
 
-    steps {
-        gradle("gradle clean test") {
-            name = "Run Unit Tests"
-            tasks = "clean test"
-            gradleWrapperPath = ""
-        }
-    }
-
-    triggers {
-        vcs {
-            branchFilter = "+:*"
-        }
-    }
-
-    requirements {
-        contains("docker.server.osType", "Linux")
+object TcDemo : VcsRoot({
+    name = "TcDemo"
+    id = VcsRootId("TcDemo")
+    url = "https://github.com/your-org/tc2.git"
+    branch = "refs/heads/main"
+    defaultBranch = "refs/heads/main"
+    authMethod = defaultMethod {
+        userName = "your-username"
     }
 })
 
-object BuildAndPackage : BuildType({
-    name = "Build and Package"
-    id = absoluteId("BuildAndPackage")
+object DemoPipeline : BuildType({
+    name = "单元测试"
+    id = absoluteId("DemoPipeline")
+
+    artifactRules = """
+        **\bin\Release\** => demodist\ReleaseLib\
+        **\obj\Release\** => demodist\ReleaseObj\
+        Report_*.html => demodist\unittest\
+        TestResult_*.xml => demodist\unittest\
+    """.trimIndent()
+    buildNumberPattern = "1.0.%build.counter%+CL%teamcity.build.vcs.number%"
+    publishArtifacts = PublishMode.SUCCESSFUL
+
+    params {
+        param("DotNetCLI_Path", """C:\Program Files\dotnet\dotnet.exe""")
+    }
 
     vcs {
-        root(DslContext.settingsRoot)
+        root(TcDemo)
+        cleanCheckout = true
+        showDependenciesChanges = true
     }
 
     steps {
-        gradle("gradle clean build") {
-            name = "Build Project"
-            tasks = "clean build"
-            gradleWrapperPath = ""
+        script {
+            name = "单元测试"
+            id = "ce_shi"
+            scriptContent = """D:\\NUnit.Console-3.22.0\\bin\\net462\\nunit3-console.exe tc2\\tc2-test\\bin\\Debug\\tc2_test.dll --result:TestResult_CL-%teamcity.build.vcs.number%.xml"""
+        }
+        powerShell {
+            name = "生成单元测试报告"
+            id = "bao_gao"
+            scriptMode = file {
+                path = """C:\TeamCity\scripts\Convert-NUnitXmlToHtml.ps1"""
+            }
+            scriptArgs = "--XmlPath TestResult_CL-%teamcity.build.vcs.number%.xml --HtmlPath Report_CL-%teamcity.build.vcs.number%.html"
+        }
+        dotnetMsBuild {
+            name = "构建项目"
+            id = "dotnet"
+            projects = "tc2/tc2.sln"
+            version = DotnetMsBuildStep.MSBuildVersion.V4
+            args = "-t:Clean;Build;Publish -p:Configuration=Release -v:detailed -property:ConsoleEncoding=utf-8"
+            sdk = "4.8"
+        }
+        dotCover {
+            name = "统计覆盖率"
+            id = "fu_gai_lv"
+            enabled = false
+            executable = """C:\\Program Files\\dotnet\\dotnet.exe"""
+            commandLineArguments = "test --framework net48"
+        }
+    }
+
+    triggers {
+        vcs {
+            branchFilter = "+:*"
+        }
+        finishBuildTrigger {
+            buildType = TcDemo_Build2.id
+        }
+    }
+
+    failureConditions {
+        executionTimeoutMin = 1
+    }
+
+    features {
+        perfmon {
+        }
+        xmlReport {
+            reportType = XmlReport.XmlReportType.NUNIT
+            rules = "TestResult_*.xml"
+            verbose = true
+        }
+    }
+})
+
+object TcDemo_Build2 : BuildType({
+    name = "Build2"
+    id = absoluteId("TcDemo_Build2")
+
+    vcs {
+        root(TcDemo)
+        cleanCheckout = true
+    }
+
+    steps {
+        script {
+            name = "示例步骤"
+            scriptContent = "echo 'Build2 step'"
         }
     }
 
@@ -74,16 +155,4 @@ object BuildAndPackage : BuildType({
             branchFilter = "+:*"
         }
     }
-
-    dependencies {
-        snapshot(UnitTests) {
-            onDependencyFailure = FailureAction.FAIL_TO_START
-        }
-    }
-
-    requirements {
-        contains("docker.server.osType", "Linux")
-    }
-
-    artifactRules = "build/libs/*.jar"
 })
--- build.gradle.kts
+++ build.gradle.kts
@@ -7,12 +7,17 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven(url = "https://download.jetbrains.com/teamcity-repository")
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
     testImplementation(kotlin("test"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+
+    // TeamCity Kotlin DSL
+    implementation("org.jetbrains.teamcity:teamcity-rest-client:2024.3")
+    implementation("org.jetbrains.teamcity:teamcity-dsl:2024.3")
 }
 
 tasks.test {
